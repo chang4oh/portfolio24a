210a7ef605b11880e6d7228eeb9f1bd999fec0f5
Chang Oh <72629686+chang4oh@users.noreply.github.com>
2024-09-02 22:28:52 -0500

    base projects section; modify contact w/ fox view, smooth plane move on arrow, more CTA

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -55,7 +55,8 @@ const Contact = () => {
   };
 
   return (
-    <section className="relative flex lg:flex-row flex-col max-container">
+    <section className="relative flex lg:flex-row flex-col 
+    max-container h-[100vh]"> {/* controls fox size h-[100vh] on view */}
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
